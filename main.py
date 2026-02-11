50d5c306c781cb62834006b35ae52a1019f9b8eb
ayuh-creator <goyalayush441@gmail.com>
2026-02-11 19:11:27 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello")
+from fastapi import FastAPI, UploadFile, File
+import os
+import shutil
+
+app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.get("/")
+def home():
+    return {"status": "Server running successfully"}
+
+@app.post("/upload-resume/")
+async def upload_resume(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "Resume uploaded successfully",
+        "filename": file.filename
+    }
